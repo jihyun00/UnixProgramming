b2f902877f5b960e5d5a7908501ea611334496db
jihyun <noblea1117@gmail.com>
2014-05-24 02:48:32 +0900

    Search File Access Authority Using Function

---
+++ ch3/ex6.c
@@ -0,0 +1,21 @@
+#include <sys/errno.h>
+#include <unistd.h>
+#include <stdio.h>
+
+extern int errno;
+
+int main(void) {
+    int per;
+
+    if(access("test.bak", F_OK) == -1 && errno == ENOENT)
+        printf("test.bak : File not exist.\n");
+
+    per = access("test.txt", R_OK);
+    if(per == 0)
+        printf("test.txt : Read permission is permmited.\n");
+    else if(per == -1 && errno == EACCES)
+        printf("test.txt : Read permission is not permitted.\n");
+
+
+    return 0;
+}
